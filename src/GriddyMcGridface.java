0258725bab4a269ce7af242bad66e979cc7a1eba
Julian Cochran <julian.p.cochran@gmail.com>
2023-12-15 14:09:05 -0500

    Can you see, it's just me, the TF of C!

---
+++ src/GriddyMcGridface.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class GriddyMcGridface {
+    private ArrayList<ArrayList<Integer>> griddyMcGridface;
+
+    public void process()   {
+        griddyMcGridface = new ArrayList<>();
+        try {
+
+        }
+        catch(Exception e)  {
+            e.printStackTrace();
+        }
+    }
+}
