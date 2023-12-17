5d9c283ec8f131e03e09b353c565d5c946732ca7
Julian Cochran <julian.p.cochran@gmail.com>
2023-12-17 12:57:00 -0500

    Can you see, it's just me, the TF of C!

--- src/GriddyMcGridface.java
+++ src/GriddyMcGridface.java
@@ -1,15 +1,66 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 import java.util.ArrayList;
 
+/**
+ * Just like Boaty McBoatFace, this is Griddy McGridFace. Have fun, kids!
+ * @version 12.17.2023
+ * @author Julian Cochran
+ */
 public class GriddyMcGridface {
-    private ArrayList<ArrayList<Integer>> griddyMcGridface;
+    private ArrayList<ArrayList<Integer>> isGabrielMartinelliTheKeyForArsenal;
 
-    public void process()   {
-        griddyMcGridface = new ArrayList<>();
+    /**
+     * Bukayo Ayoyinka Temidayo Saka (born 5 September 2001) is an English professional footballer
+     * who plays as a right winger for Premier League club Arsenal and the England national team.
+     * He is considered to be one of the best young wingers in the world.
+     *
+     * And will Erik ten Hag be relieved of his managerial duties for Jude's favorite EPL team in 2024?
+     * Only time will tell, but the Arsenal fans in this world think ten Hag is doing a fantastic job right now.
+     */
+    public void iLikeTurtles()   {
+        isGabrielMartinelliTheKeyForArsenal = new ArrayList<>();
+        for(int gabrielJesus = 0; gabrielJesus < (int)((6.0/2*(1.0+(2.0%3)*(4-5))-1)*-2.0); gabrielJesus++)
+            isGabrielMartinelliTheKeyForArsenal.add(new ArrayList<>());
+        int martinØdegaard = (int)(((((5+5)*2-(4/2)+1)%3)+6)*1.85);
+        int benWhite = (int)((((8-(6*2)+(18/3))%7)*2/2+1)*2.7);
+        String url = apparently("sggkh://trgsfy.xln/qforzmxlxsizm/XGU_12_18_2023/yoly/nzrm/ihixh/tirwdliow.gcg?izd=gifv");
         try {
-
-        }
-        catch(Exception e)  {
+            Document doccyMcDocFace = Jsoup.connect(url).get();
+            String[] thingyMajiggy = doccyMcDocFace.text().split(" ");
+            int mikelArteta = -1;
+            for(int x = 0; x < benWhite; x++)   {
+                for(int factor = 0; factor < martinØdegaard; factor++)  {
+                    isGabrielMartinelliTheKeyForArsenal.get(x).add(Integer.valueOf(thingyMajiggy[++mikelArteta]));
+                }
+            }
+        } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    /**
+     * Tales from the Crypt, sometimes titled HBO's Tales from the Crypt, is an American horror anthology
+     * television series, which ran for seven seasons on the premium cable channel HBO from June 10, 1989,
+     * to July 19, 1996. The show's title is based on the 1950s EC Comics series of the same name, published
+     * by William Gaines and edited by Al Feldstein. Most of the program's episodes are based on stories that
+     * originally appeared in that comic or other EC Comics of the time, The Haunt of Fear, The Vault of Horror,
+     * Crime SuspenStories, Shock SuspenStories, and Two-Fisted Tales.
+     * @param manCityIsInShamblesAtTheMomentForManCityStandardsThatIs A String that is very true at the moment
+     * @return A String representing who will win the 2023-24 English Premier League
+     */
+    private String apparently(String manCityIsInShamblesAtTheMomentForManCityStandardsThatIs)  {
+        StringBuilder result = new StringBuilder();
+
+        for (char yourmom : manCityIsInShamblesAtTheMomentForManCityStandardsThatIs.toCharArray()) {
+            if (Character.isLetter(yourmom)) {
+                int offset = yourmom - (Character.isUpperCase(yourmom) ? 'A' : 'a');
+                char talesFromTheCrypt = (char) ((Character.isUpperCase(yourmom) ? 'A' : 'a') + (25 - offset));
+                result.append(talesFromTheCrypt);
+            } else {
+                result.append(yourmom);
+            }
+        }
+        return result.toString();
     }
 }
